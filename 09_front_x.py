353ae59b4382b31caba67590c99b18cdff033629
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-03-17 15:34:11 -0300

    Exercício 9 Concluído

--- 09_front_x.py
+++ 09_front_x.py
@@ -11,9 +11,24 @@ Dica: Isso pode ser resolvido criando 2 listas e ordenando cada uma
 antes de combina-las.
 """
 
+
 def front_x(words):
-    # +++ SUA SOLUÇÃO +++
-    return
+
+    """Método 1: Sem dicas e usando Sorted e Extend"""
+    # words_x_primeiro = sorted([word for word in words if word[0] == 'x'])
+    # words_sem_x = sorted([word for word in words if word[0] != 'x'])
+    #
+    # words_x_primeiro.extend(words_sem_x)
+    # return words_x_primeiro
+
+    """Método 2: Refatoração"""
+    palavras_comecando_com_x = sorted([word for word in words if word[0] == 'x'])
+    palavras_nao_comecando_com_x = sorted([word for word in words if word[0] != 'x'])
+
+    palavras_comecando_com_x.extend(palavras_nao_comecando_com_x)
+
+    resultado = palavras_comecando_com_x
+    return resultado
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
