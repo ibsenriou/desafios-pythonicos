98358d907a9254d3cda75bf0cf82eb7fa32a8d90
Ibsen Gabriel Santos <ibsenriou@hotmail.com>
2022-03-17 16:24:57 -0300

    Exercício 12 Concluído

--- 12_linear_merge.py
+++ 12_linear_merge.py
@@ -8,10 +8,12 @@ modificar as listas recebidas.
 A sua solução deve rodar em tempo linear, ou seja, deve fazer uma
 única passagem em cada uma das listas.
 """
+import heapq
+
 
 def linear_merge(list1, list2):
-    # +++ SUA SOLUÇÃO +++
-    return
+    merge = heapq.merge(list1, list2)
+    return list(merge)
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
